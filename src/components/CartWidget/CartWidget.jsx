a0827426908c8754cfec3bb7e2622971c69f2b9c
Santino Scannapieco <santinoscannapieco@gmail.com>
2023-11-30 21:26:45 -0300

    Corrección de carpetas

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,11 +1,14 @@
 import carrito from '../../assets/cart-shopping-white-2xl.svg'
 
+import { FaShoppingCart } from "react-icons/fa";
+
 const CartWidget = () => {
     const counter = 0
 
     return (
-        <a href="#" className="flex items-center relative w-11 h-11">
-            <img src={carrito} alt="carrito" />
+        <a href="#" className="flex items-center relative">
+            {/* <img src={carrito} alt="carrito" /> */}
+            <FaShoppingCart className='text-slate-50 text-5xl h-11 mr-2' />
 
             <span className="absolute bottom-0 right-0 bg-red-700 text-white rounded-full px-2 py-1 text-xs">
                 {counter}
