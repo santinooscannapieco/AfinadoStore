b897472c9c33642df831cd63424be448ce7828d5
Santino Scannapieco <santinoscannapieco@gmail.com>
2023-12-04 12:50:34 -0300

    Corrección de la primera entrega

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <Navbar />
 
-      <ItemListContainer />
+      <ItemListContainer greeting={"Productos"}/>
     </>
   )
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,12 +1,13 @@
 import Tarjetas from '../Tarjetas/Tarjetas'
 
 
-const ItemListContainer = () => {
+const ItemListContainer = ( {greeting} ) => {
 
     return (
         <section className='container m-auto py-20'>
-            <h2 className="text-4xl font-bold">Productos</h2>
+            <h2 className="text-4xl font-bold">{greeting}</h2>
             <hr className='my-6 border-black'/>
+            
             <div className='flex gap-10'>
                 <Tarjetas titulo={"Guitarra acústica"} texto={"Esta es una guitarra acústica"}/>
                 <Tarjetas titulo={"Cuerda guitarra"} texto={"Encordado Guitarra"}/>
